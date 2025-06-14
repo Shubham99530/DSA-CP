c78949c197e51d59ebe0ba282f01e2efd1d6aa2a
Shubham99530 <shubham21290@iiitd.ac.in>
2025-06-14 15:12:07 +0530

    used another method for anagram question

--- Strings/valid_anagram.cpp
+++ Strings/valid_anagram.cpp
@@ -1,27 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> sp;
-        unordered_map<char,int> tp;
-
-        if(s.size() != t.size())
-        {
-            return false;
-        }
-        for(int i=0; i<s.size(); i++)
-        {
-            sp[s[i]]++;
-            tp[t[i]]++;
-        }
-        int j=0;
-        while(sp[s[j]] == tp[s[j]])
-        {
-            j++;
-            if(j == s.size())
-            {
-                return true;
-            }
-        }
-        return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
