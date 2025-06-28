34bc1c1edc848ccdd369f64adb1f84a4deb0843b
Shubham99530 <shubham21290@iiitd.ac.in>
2025-06-28 23:07:42 +0530

    Sliding window pattern almost done

---
+++ Sliding_window/Longest_Substring_with_k_uniques.cpp
@@ -0,0 +1,38 @@
+// User function template for C++
+
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        // your code here
+        return atmost(s,k);
+    }
+    int atmost(string &s, int k)
+    {
+        unordered_map<char,int>mp;
+        // int count=0;
+        int ans=-1;
+        int r=0;
+        int l=0;
+        while(r<s.size())
+        {
+            mp[s[r]]++;
+            while(mp.size() > k && l<=r)
+            {
+                mp[s[l]]--;
+                // l++;
+                if(mp[s[l]]==0)
+                {
+                    mp.erase(s[l]);
+                }
+                l++;
+            }
+            r++;
+            if(mp.size() == k)
+            {
+                ans = max(ans,r-l);
+            }
+        }
+        
+        return ans;
+    }
+};
