265e9a84cd1eb4ebd6d4fa5804f05f4cd44f2959
Shubham99530 <shubham21290@iiitd.ac.in>
2025-07-13 00:53:58 +0530

    started heap questions

---
+++ Heaps/Kth_largest_element_in_an_array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>min_heap;
+        int i=0;
+        while(i<k)
+        {
+            min_heap.push(nums[i]);
+            i++;
+        }
+        while(i<nums.size())
+        {
+            if(nums[i] > min_heap.top())
+            {
+                min_heap.pop();
+                min_heap.push(nums[i]);
+            }
+            i++;
+        }
+        return min_heap.top();
+    }
+};
