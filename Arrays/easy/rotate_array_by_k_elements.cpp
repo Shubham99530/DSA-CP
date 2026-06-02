252b66f8b1d2d075494d3ea69bbb1520e41ff727
Shubham99530 <sy5455025@gmail.com>
2026-06-02 22:31:07 +0530

    did two more questions just to be consistent with my DSA journey this time even though I want to sleep

---
+++ Arrays/easy/rotate_array_by_k_elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void reverse(vector<int>& nums,int p, int k)
+    {
+        int i=p;
+        int j=k;
+        while(i<j)
+        {
+            swap(nums[i++],nums[j--]);
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        if (k > nums.size())
+        {
+            k = k % nums.size();
+        }
+        reverse(nums,0,nums.size()-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.size()-1);
+    }
+};
